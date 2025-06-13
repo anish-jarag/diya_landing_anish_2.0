5f0ae3c17f127dd5a410f5ea86b0aea344af8c71
anish-jarag <anishjarag16@gmail.com>
2025-06-14 00:06:38 +0530

    Initial commit

---
+++ src/navigation.ts
@@ -0,0 +1,79 @@
+import { getPermalink, getHomePermalink } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    { text: 'Home', href: getHomePermalink() },
+    { text: 'Features', href: getPermalink('/#features') },
+    {
+      text: 'Why DIYA',
+      links: [
+        { text: 'Student Experience', href: '#student' },
+        { text: 'Teacher Experience', href: '#teacher' },
+        { text: 'Mobile Experience', href: '#mobile' },
+      ],
+    },
+    { text: 'How it works', href: getPermalink('/howitworks') },
+    { text: 'About us', href: getPermalink('/about') },
+  ],
+  actions: [{ text: 'Contact Us', href: getPermalink('/contact') }],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Enterprise', href: '#' },
+        { text: 'Customer stories', href: '#' },
+        { text: 'Pricing', href: '#' },
+        { text: 'Resources', href: '#' },
+      ],
+    },
+    {
+      title: 'Platform',
+      links: [
+        { text: 'Developer API', href: '#' },
+        { text: 'Partners', href: '#' },
+        { text: 'Atom', href: '#' },
+        { text: 'Electron', href: '#' },
+        { text: 'AstroWind Desktop', href: '#' },
+      ],
+    },
+    {
+      title: 'Support',
+      links: [
+        { text: 'Docs', href: '#' },
+        { text: 'Community Forum', href: '#' },
+        { text: 'Professional Services', href: '#' },
+        { text: 'Skills', href: '#' },
+        { text: 'Status', href: '#' },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '#' },
+        { text: 'Blog', href: '#' },
+        { text: 'Careers', href: '#' },
+        { text: 'Press', href: '#' },
+        { text: 'Inclusion', href: '#' },
+        { text: 'Social Impact', href: '#' },
+        { text: 'Shop', href: '#' },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+  ],
+  footNote: '© 2025 DIYA Edutech | All rights reserved',
+};
